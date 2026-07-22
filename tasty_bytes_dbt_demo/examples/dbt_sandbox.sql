7b093650bef939d7403b6a827b0db9d7be01c867
maxim-austin <maksim_krupenin@epam.com>
2026-07-22 00:27:13 +0000

    New branch test-branch created from main

--- tasty_bytes_dbt_demo/examples/dbt_sandbox.sql
+++ tasty_bytes_dbt_demo/examples/dbt_sandbox.sql
@@ -1,3 +1,19 @@
 SHOW TABLES IN DATABASE tasty_bytes_dbt_db;
 
 SHOW VIEWS IN DATABASE tasty_bytes_dbt_db;
+
+CREATE USER IF NOT EXISTS github_actions_service_user
+  TYPE = SERVICE
+  WORKLOAD_IDENTITY = (
+    TYPE = OIDC
+    ISSUER = 'https://token.actions.githubusercontent.com',
+    SUBJECT = 'repo:github.com/maxim-austin/getting-started-with-dbt-on-snowflake:environment:prod'
+  )
+  DEFAULT_ROLE = ACCOUNTADMIN
+  COMMENT = 'Service user for GitHub Actions';
+
+GRANT ROLE ACCOUNTADMIN TO USER github_actions_service_user;
+
+ALTER USER github_actions_service_user SET DEFAULT_WAREHOUSE = 'TASTY_BYTES_DBT_WH';  
+
+
